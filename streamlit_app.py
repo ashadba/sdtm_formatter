895a23290fcb7f0caed18daad4fb1c8b9772d157
Ashadba <ashajohn2021@outlook.com>
2023-06-25 10:23:37 -0500

    Added config.toml to change default theme of streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ with st.sidebar:
 
 if section == 'SDTM Data Mapper':
     sponsor_data = fun.get_sponsor()
-    uploaded_file = st.file_uploader("Upload exported data file")
+    uploaded_file = st.file_uploader("Upload data file")
 
     if uploaded_file is not None:
         data_df = fun.read_file(sponsor_data, uploaded_file)
